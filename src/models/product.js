98767f13e9e3de49004f253ec7ee78f9e0d99fa9
Avelexis <alsydoruk642@gmail.com>
2026-09-13 10:36:29 +0200

    add product

---
+++ src/models/product.js
@@ -0,0 +1,31 @@
+import { Schema, model } from 'mongoose';
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: ['books', 'electronics', 'clothing', 'other'],
+      default: 'other',
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Product = model('Product', productSchema);
