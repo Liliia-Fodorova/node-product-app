7b40cd03a312d58d51ce7dc7481ac1aad55fa87e
Liliia Fodorova <nevskay22@gmail.com>
2026-09-13 12:25:18 +0200

    add deletProduct

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -36,3 +36,14 @@ export const updateProduct = async (req, res)=>{
 
 
 }
+
+export const deleteProduct = async(req, res) => {
+    const {productId} = req.params;
+
+    const product = await Product.findOneAndDelete({_id: productId});
+
+    if(!product){
+        throw createHttpError(404, 'Product not found');
+}
+   res.status(200).json(product);
+}
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { createProduct, getProducts, getProductsById, updateProduct } from "../controllers/productsController.js"
+import { createProduct, deleteProduct, getProducts, getProductsById, updateProduct } from "../controllers/productsController.js"
 const router = Router()
 router.get('/products', getProducts)
 router.get('/products/:productId', getProductsById);
 router.post('/products', createProduct);
 router.patch('/products/:productId', updateProduct);
+router.delete('/products/:productId', deleteProduct);
 export default router
